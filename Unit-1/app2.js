fc6b3d28dc05c6d96831e4a82ce80fe40902aea5
Lisa Carlson <lisa.carlson85@gmail.com>
2015-11-05 10:21:41 -0700

    07 complete

--- Unit-1/app2.js
+++ Unit-1/app2.js
@@ -5,8 +5,6 @@ app.controller("MyController", function ($scope) {
   $scope.player2 = 0;
   $scope.isServing1 = false;
   $scope.isServing2 = false;
-  $scope.turn1 = true;
-  $scope.turn2 = false;
   $scope.click = 0;
   $scope.gameOver = false;
   $scope.pickRandomNumber = function(){
@@ -18,27 +16,21 @@ app.controller("MyController", function ($scope) {
   $scope.playPlayer1 = function(){
     $scope.player1++;
     $scope.click++;
-    if ($scope.click % 2 == 0 && $scope.turn1 == true) {
+    if ($scope.click % 2 == 0) {
       $scope.isServing1 = true;
-      $scope.turn1 = false;
-      $scope.turn2 = true;
     }
-    
     else {
       $scope.isServing1 = false;
     }
     if ($scope.player1 == 11) {
       $scope.gameOver = true;
-
     }
   }
   $scope.playPlayer2 = function(){
     $scope.player2++;
     $scope.click++;
-    if ($scope.click % 2 == 0 && $scope.turn2 == true) {
+    if ($scope.click % 2 == 0) {
       $scope.isServing2 = true;
-      $scope.turn2 = false;
-      $scope.turn1 = true;
     }
     else {
       $scope.isServing2 = false;
@@ -46,11 +38,33 @@ app.controller("MyController", function ($scope) {
     if ($scope.player2 == 11) {
       $scope.gameOver = true;
     }
-  }
+  };
   $scope.reset = function() {
     $scope.player1 = 0;
     $scope.player2 = 0;
     $scope.isServing = false;
     $scope.gameOver = false;
-  }
+  };
+  
+
 })
+
+app.controller("MyController2", function ($scope) {
+  $scope.contacts = [{name: 'Harry Potter', email: 'harry@hogwarts.edu', phone: '467-982-7655'},
+                      {name: 'Arya Stark', email: 'arya@winterfell.com', phone: '788-973-9373'},
+                      {name:'Edward Scissorhands', email: 'ed@victorprice.gov', phone:'857-484-3837'}];
+  $scope.newName = '';
+  $scope.newEmail = '';
+  $scope.newPhone = '';
+  $scope.addContact = function(){
+    $scope.newContact = {name: $scope.newName, email: $scope.newEmail, phone: $scope.newPhone};
+    $scope.contacts.push($scope.newContact);
+    console.log('click')
+  }
+
+  })
+
+
+
+
+
